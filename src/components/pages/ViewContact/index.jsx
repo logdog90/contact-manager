07e296f48e2869f784671e2d08b7bc4e7a52b1cb
LogDog <logdogkrzn@gmail.com>
2024-04-04 15:36:22 +1000

    Вёрстка страницы с контактом

--- src/components/pages/ViewContact/index.jsx
+++ src/components/pages/ViewContact/index.jsx
@@ -1,6 +1,50 @@
+import { Link } from "react-router-dom"
+import './styles.css'
+
 const ViewContact = () => {
   return (
-    <h3>Контакт</h3>
+    <>
+      <section className="view-contact">
+        <div className="container">
+          <h3 className="mb-4">Контакт</h3>
+          <div className="row mb-4">
+            <div className="col-md-4">
+              <img 
+                src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" 
+                alt="Фото"
+                className="view-contact-photo"
+              />
+            </div>
+            <div className="col-md-8">
+              <ul className="list-group mb-3">
+                <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                  <span><b>ФИО:</b></span>
+                  <span>Иванов Иван Иванович</span>
+                </li>
+                <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                  <span><b>Должность:</b></span>
+                  <span>Сантехник</span>
+                </li>
+                <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                  <span><b>Телефон:</b></span>
+                  <span>+7 (984) 123-32-12</span>
+                </li>
+              </ul>
+              <div className="row">
+                <div className="col text-start">
+                  <Link 
+                    to="/contact"
+                    className="btn btn-warning"
+                  >
+                    Назад
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
